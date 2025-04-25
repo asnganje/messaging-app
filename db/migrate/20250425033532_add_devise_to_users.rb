234dfca5415c188acd6d89010f4e06060b2cc286
asnganje <abd.nganje@gmail.com>
2025-04-25 07:05:27 +0300

    add user sign in and out setup

---
+++ db/migrate/20250425033532_add_devise_to_users.rb
@@ -0,0 +1,14 @@
+class AddDeviseToUsers < ActiveRecord::Migration[8.0]
+  def change
+    # Only add encrypted_password and other Devise columns
+    add_column :users, :encrypted_password, :string, null: false, default: ""
+
+    add_column :users, :reset_password_token,   :string
+    add_column :users, :reset_password_sent_at, :datetime
+
+    add_column :users, :remember_created_at, :datetime
+
+    # Indexes
+    add_index :users, :reset_password_token, unique: true
+  end
+end
